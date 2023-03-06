8363960255850d1336e6a9fe0eedc3e70ace9db1
Roberto Martins <robertomartins@Robertos-MacBook-Pro.local>
2023-03-06 15:32:33 -0300

    Adding Selection sort algorithm implemenatation

--- algo.py
+++ algo.py
@@ -22,7 +22,6 @@ for i in range(100):
 
 val, count = np.unique(ans, return_counts=True)
 count_a = count/np.sum(count)
-print(val, count_a, count, np.sum(count))
 
 
 ## Bubble sort implementation
@@ -51,7 +50,37 @@ import numpy as np
 
 a = np.random.randint(50, size=(1,10))
 a = a[0].tolist()
-print(a)
-print(bubb([100, 90,80, 70 ,60 ,50,40,30,20,10]))
+#print(a)
+#print(bubb([100, 90,80, 70 ,60 ,50,40,30,20,10]))
 #Worst case n * (n-1) = n**2 - n
 #Best case n-1 = n
+
+
+def selection_sort(array):
+    #Selection sort algo: parses through array and finds lowest element, setting it to its position
+    #set min to fist el
+    compar = 0
+    swaps = 0
+    #parse through list n times
+    for j in range(len(array)-1):
+        minimum = array[j]
+        swapp = False
+        #parse through list to check numbers
+        for i in range(j+1, len(array)):
+            #compares to min
+            compar += 1
+            if minimum > array[i]:
+                minimum = array[i]
+                index = i
+                swapp = True
+
+        #swaps values
+        if swapp is True:
+            swaps += 1
+            tmp = array[index]
+            array[index] = array[j]
+            array[j] = tmp
+        print(array)
+    return array, compar, swaps
+        
+print(selection_sort([70, 40, 69, 80, 12, 14, 34]))
